1a6319708150d530b0ec083a757cc61c3087087a
Max <example@gmail.com>
2022-06-15 18:15:50 +0300

    fix1

---
+++ JSlessons/js/6.js
@@ -0,0 +1,44 @@
+
+// let num = 20;
+
+// function showFirstMessage(text){
+//     console.log(text);
+//     let num = 10;
+//     console.log(num);
+// }
+
+// showFirstMessage("Hi Bro!");
+// console.log(num);
+
+// function calc(a, b){
+//     return (a + b);
+// }
+
+// console.log(calc(4,3));
+
+
+// function ret(){
+//     let num = 50;
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function() {
+//     console.log("Hello");
+// };
+// logger();
+
+// const calcu = (a, b) => a + b; 
+
+
+const usdCurr = 28;
+const euroCurr = 32;
+
+let suma = +prompt("Уведіть кількість euro/usd --> ");
+
+function convert(amount, curr){
+    console.log(curr * amount);
+}
+
