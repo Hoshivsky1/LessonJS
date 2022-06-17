16b2b15fb19104510489e15fe1b13385ff073c59
Max <example@gmail.com>
2022-06-17 15:00:30 +0300

    ObjectCopyMethod

--- JSlessons/js/massive.js
+++ JSlessons/js/massive.js
@@ -6,11 +6,11 @@ function compareNum(a, b) {
     return a - b;
 }
 
-// arr.forEach(function(item, i, arr) {
-//     console.log(`${i}: ${item} в середині масива ${arr}`);
-// });
+arr.forEach(function(item, i, arr) {
+    console.log(`${i}: ${item} в середині масива ${arr}`);
+});
 
-// const str = prompt('', '');
-// const products = str.split(', ');
-// products.sort();
-// console.log(products.join('; '));
+const str = prompt('', '');
+const products = str.split(', ');
+products.sort();
+console.log(products.join('; '));
